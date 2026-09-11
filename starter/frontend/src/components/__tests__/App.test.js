e97bf214d1e377a6df2dc650c1ff477e07e06429
SaideepTambe1191 <saideeptambe.nbnstic.it>
2026-09-11 11:10:10 +0530

    simulate test failure

--- starter/frontend/src/components/__tests__/App.test.js
+++ starter/frontend/src/components/__tests__/App.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import App from '../../App';
 
-const movieHeading = process.env.FAIL_TEST ? 'WRONG_HEADING' : 'Movie List';
+const movieHeading = 'WRONG_HEADING';
 
 test('renders Movie List heading', () => {
   render(<App />);
